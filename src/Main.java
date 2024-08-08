02faf6b9867a1d991efa2e3d5e4c0ee7d0dd3c67
Faby4023 <143666861+Faby4023@users.noreply.github.com>
2024-08-07 21:19:17 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Questão 1
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite sua idade: ");
+        int idade = s.nextInt();
+
+
+        System.out.print("Digite sua idade em anos: ");
+        int anos = s.nextInt();
+
+        System.out.print("Digite sua idade em meses: ");
+        int meses = s.nextInt();
+
+        System.out.print("Digite sua idade em dias:");
+        int dias = s.nextInt();
+        int idadeEmDias = anos * 365 + meses * 30 + dias;
+        System.out.println("A idade em dias é: " + idadeEmDias + " dias");
+
+    }
+}
---
+++ src/Questão6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Questão6 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int numero = s.nextInt();
+    }
+}
